002044b85bcc7f374b4ba56ce86d34362c628988
Dor-Col <dorcol@wp.pl>
2023-02-26 20:39:23 +0100

    added tests

---
+++ Studenttests.cs
@@ -0,0 +1,30 @@
+using System;
+using ChallengeApp;
+using Xunit;
+
+namespace Challenge.tests
+{
+    public class Studenttests
+    {
+        [Fact]
+        public void Test1()
+        {
+            //arrage
+            var student = new SavedStudent("Grzegorz", "Kowalski");
+            
+            
+            student.AddGrade(1);
+            student.AddGrade(1.5);
+            student.AddGrade(5);   
+                  
+
+            //act
+            var result = student.GetStatistics();
+
+            //assert
+            Assert.Equal(2.5, result.Average);
+            Assert.Equal(5, result.High);
+            Assert.Equal(1, result.Low);
+        }
+    }
+}
